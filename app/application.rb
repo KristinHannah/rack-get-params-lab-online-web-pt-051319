8644ce1d0def4c6cd64b1cdd7eb76a4842471a33
Kristin Kraemer <kristinhannahk@gmail.com>
2019-08-04 14:37:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -38,9 +38,9 @@ class Application
   def add_search(search_term)
    if @@items.include?(search_term)
      @@cart << search_term 
-     resp.write "added #{search_term}"
+     return "added #{search_term}"
    else 
-     resp.write "We don't have that item"
+     return "We don't have that item"
    end 
   end 
      
